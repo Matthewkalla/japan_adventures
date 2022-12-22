fcbb3ce39a2de057bccff3a4c949420f0c7b70d4
Matthewkalla <mparsons077@gmail.com>
2022-12-21 22:08:00 -0800

    deleted the api key

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from flask_app import app
 from flask_app.controllers import users_controller, places_controller
 
 if __name__ == "__main__":
-    app.run(debug=True) #this runs the app initialized in our init
+    app.run(debug=True, host="0.0.0.0") #this runs the app initialized in our init
